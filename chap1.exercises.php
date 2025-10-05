6e3ba9a476e57bdb1ad1608fa95aed9b5dea4e75
Alistair Mann <al+inpSep2025@pectw.net>
2025-10-05 17:38:49 +0100

    Chapter 1 exercises
    Exercises 2 asks for a graph of how close the hamming distance is for random
    starting patterns. The same patterns are shown in chap1.exercises.php. Posted
    here as an ODS file
    
    Exercises 3 and 4 ask for analysing how an unclamped network behaves, then
    repeat with the central TAN clamped first to 0 and then 1. chap1.exercises.php
    does this by reporting the stats from multiple runs

---
+++ chap1.exercises.php
@@ -0,0 +1,301 @@
+<?php
+declare(strict_types=1);
+
+/*
+  Chapter 1 Exercises 3 & 4 look at how clamping (not, or 0, or 1) does or
+  does not sway outcomes
+*/
+
+require_once('defines.php');
+require_once('neural-objects.php');
+require_once('neural-objects-bodges.php');
+
+//
+// Defines only for C1Exercises
+//
+// Indicies for recording alternative outcomes as well as STABLE,
+// TOODEEP, and CYCLE2
+define("STMS_STABLE_T", 501);
+define("STMS_STABLE_H", 502);
+// Index for storing the starting pattern for that test
+define("STMS_INIT_VALS", 503);
+//
+// How many times to test one pattern for alternative resolutions
+define("PASSES", 5000);
+// Maximum cycles to wait for resolution to happen
+define("HARDLOOPLIMIT", 6);
+
+//
+// Termination conditions for exercises 3 and 4
+// T
+define("TESTExercise3n403a", " 111 \n");
+define("TESTExercise3n403b", " 010 \n");
+define("TESTExercise3n403c", " 010\n");
+//H
+define("TESTExercise3n404a", " 101 \n");
+define("TESTExercise3n404b", " 111 \n");
+define("TESTExercise3n404c", " 101\n");
+
+//
+// Setup the network
+function test_exampleExercise3n4($clamped) {
+    //
+    // Set up TANs to have taught set as provided by T and H
+    $tan11 = new ToyAdaptiveNode_test_18;
+    $tan11->setNumberInputs(4);
+    $tan11->setUsage(USAGE_USE);
+    $tan11->setTaughtSets([], [[1, 1, 0, 1], [1, 0, 1, 0]]); // ,TH
+    $tan12 = new ToyAdaptiveNode_test_18;
+    $tan12->setNumberInputs(4);
+    $tan12->setUsage(USAGE_USE);
+    $tan12->setTaughtSets([[1, 0, 1, 1]], [[1, 1, 1, 1]]); // H,T
+    $tan13 = new ToyAdaptiveNode_test_18;
+    $tan13->setNumberInputs(4);
+    $tan13->setUsage(USAGE_USE);
+    $tan13->setTaughtSets([], [[0, 1, 1, 1], [1, 0, 1, 0]]); // ,TH
+    $tan21 = new ToyAdaptiveNode_test_18;
+    $tan21->setNumberInputs(4);
+    $tan21->setUsage(USAGE_USE);
+    $tan21->setTaughtSets([[0, 1, 1, 0]], [[1, 1, 1, 1]]); // T,H
+    $tan22 = new ToyAdaptiveNode_test_18;
+    $tan22->setNumberInputs(4);
+    $tan22->setUsage(USAGE_USE);
+    $tan22->setTaughtSets([], [[1, 0, 1, 0], [0, 1, 0, 1]]); // ,TH
+    $tan23 = new ToyAdaptiveNode_test_18;
+    $tan23->setNumberInputs(4);
+    $tan23->setUsage(USAGE_USE);
+    $tan23->setTaughtSets([[1, 1, 0, 0]], [[1, 1, 1, 1]]); // T,H
+    $tan31 = new ToyAdaptiveNode_test_18;
+    $tan31->setNumberInputs(4);
+    $tan31->setUsage(USAGE_USE);
+    $tan31->setTaughtSets([[0, 0, 1, 1]], [[1, 1, 0, 1]]); // T,H
+    $tan32 = new ToyAdaptiveNode_test_18;
+    $tan32->setNumberInputs(4);
+    $tan32->setUsage(USAGE_USE);
+    $tan32->setTaughtSets([[1, 1, 1, 0]], [[0, 1, 0, 1]]); // H,T
+    $tan33 = new ToyAdaptiveNode_test_18;
+    $tan33->setNumberInputs(4);
+    $tan33->setUsage(USAGE_USE);
+    $tan33->setTaughtSets([[1, 0, 0, 1]], [[0, 1, 1, 1]]); // T,H
+
+    //
+    // Set up connections
+    $tan11->setFromInputNAsTan(0, [$tan13, $tan31, $tan12, $tan21]);
+    $tan12->setFromInputNAsTan(0, [$tan11, $tan32, $tan13, $tan22]);
+    $tan13->setFromInputNAsTan(0, [$tan12, $tan33, $tan11, $tan23]);
+
+    $tan21->setFromInputNAsTan(0, [$tan23, $tan11, $tan22, $tan31]);
+    $tan22->setFromInputNAsTan(0, [$tan21, $tan12, $tan23, $tan32]);
+    $tan23->setFromInputNAsTan(0, [$tan22, $tan13, $tan21, $tan33]);
+
+    $tan31->setFromInputNAsTan(0, [$tan33, $tan21, $tan32, $tan11]);
+    $tan32->setFromInputNAsTan(0, [$tan31, $tan22, $tan33, $tan12]);
+    $tan33->setFromInputNAsTan(0, [$tan32, $tan23, $tan31, $tan13]);
+
+    $tanlist = array($tan11, $tan12, $tan13,
+                     $tan21, $tan22, $tan23,
+                     $tan31, $tan32, $tan33);
+    //
+    // Exercise invites us to use no clamp, clamped to 0, and clamped to 1
+    if ($clamped === UNCLAMPED) {
+        echo "No clamp\n";
+        $tan31->setClampedValue(UNCLAMPED);
+    } else if ($clamped === CLAMPED0) {
+        echo "0 clamp\n";
+        $tan31->setClampedValue(CLAMPED0);
+    } else if ($clamped === CLAMPED1) {
+        echo "1 clamp\n";
+        $tan31->setClampedValue(CLAMPED1);
+    }
+
+    //
+    // Same starting set given on p16, second and third set match those
+    // I used for Exercise 2
+    $startingSets = [[0, 0, 0, 0, 0, 0, 1, 0, 0],
+                     [0, 0, 0, 1, 1, 1, 0, 0, 0],
+                     [1, 0, 0, 0, 0, 1, 0, 1, 0],
+                     [1, 1, 1, 1, 1, 1, 0, 1, 1],
+
+                     [0, 0, 0, 1, 1, 1, 0, 0, 1],
+                     [0, 0, 0, 1, 1, 1, 0, 1, 1],
+                     [0, 0, 1, 0, 0, 1, 0, 1, 1],
+                     [0, 1, 0, 0, 1, 0, 0, 1, 1],
+                     [0, 1, 0, 1, 1, 1, 1, 0, 0],
+                     [0, 1, 1, 1, 1, 0, 0, 0, 0],
+
+                     [1, 0, 1, 0, 0, 0, 1, 0, 0],
+                     [1, 0, 1, 0, 1, 0, 1, 1, 1],
+                     [1, 0, 1, 0, 1, 1, 1, 1, 1],
+                     [1, 0, 1, 1, 1, 1, 1, 1, 1],
+                     [1, 1, 0, 0, 1, 0, 0, 1, 0],
+                     [1, 1, 0, 0, 1, 1, 0, 1, 1],
+                     [1, 1, 1, 0, 0, 0, 0, 1, 0],
+                     [1, 1, 1, 1, 0, 1, 1, 0, 1]];
+
+    runNetwork($startingSets, $tanlist);
+    echo "Completed " . __FUNCTION__ . "\n";
+}
+
+//
+// Actually run the network and aggregate statistics over multiple passes
+function runNetwork($startingSets, $tanlist) {
+    foreach ($startingSets as $initialVals) {
+        // Test each starting set several times in turn
+
+        // Make ready to hold the original patterns and stats, +1 each time
+        // we have that outcome. T and H independent of STABLE, TOODEEP,
+        // and CYCLE2
+        $localResult = array(STMS_INIT_VALS => $initialVals,
+                             STMS_STABLE => 0, STMS_TOODEEP => 0,
+                             STMS_CYCLE2 => 0, STMS_STABLE_T => 0,
+                             STMS_STABLE_H => 0);
+
+        // Repeat the same starting point PASSES times to account for
+        // undefineds leading to differing resolutions
+        for ($i = 0; $i < PASSES; $i++) {
+            // Setup for multiple runs in a pass
+            $stms = array();
+            $hardLoopLimit = HARDLOOPLIMIT;
+            // Initialise the network
+            $index = 0;
+            foreach($tanlist as $tan) {
+                $tan->initialise($initialVals[$index++]);
+            }
+
+            // Get and record the networks "0-state"
+            $finished = false;
+            $stmsHistory = array();
+            foreach($tanlist as $tan) {
+                $stms[] = $tan->getF(__FUNCTION__);
+            }
+            $stmsHistory[] = $stms;
+
+            do {
+                // Run the network at least once and until finished, either by
+                // timing out, or reaching a 0 cycle or 1 cycle state
+
+                // Setup for single run in one pass
+                $stms = array();
+
+                // Assess all the inputs first
+                foreach($tanlist as $tan)
+                    $tan->runInputs();
+
+                // Only once all inputs assessed, obtain and store node outputs
+                foreach($tanlist as $tan) {
+                    $tan->runSlowOutput();
+                    $stms[] = $tan->getF(__FUNCTION__);
+                }
+                $stmsHistory[] = $stms;
+
+                //
+                // Now check for termination
+                //
+                // We could check against all previous short term memory states for
+                // a match. However, we could also see an intermediate cycle with
+                // an undefined state release us at the next go through. Skipped
+                // for now
+                // And also terminate if we've run away
+                if ($hardLoopLimit-- === -1)
+                    // If we're in too deep, we don't need to check the other
+                    // conditions
+                    $finished = STMS_TOODEEP;
+
+                else if (($sz = sizeof($stmsHistory)) > 1) {
+                    if ($stmsHistory[$sz - 1] === $stmsHistory[$sz - 2]) {
+                        // This will match on a 1 cycle stable state
+                        $finished = STMS_STABLE;
+
+                    } elseif ($sz > 3) {
+                        // This will match on a 2 cycle stable state per page18
+                        if (($stmsHistory[$sz - 1] === $stmsHistory[$sz - 3]) &&
+                            ($stmsHistory[$sz - 2] === $stmsHistory[$sz - 4])) {
+                            $finished = STMS_CYCLE2;
+                        }
+                    }
+                }
+            } while ($finished === false);
+
+            //
+            // Record why we finished, and whether we finished on a
+            // prototype pattern
+            $localResult[$finished]++;
+            if ($stms === [1, 1, 1, 0, 1, 0, 0, 1, 0])
+                $localResult[STMS_STABLE_T] = $localResult[$finished];
+            else if ($stms === [1, 0, 1, 1, 1, 1, 1, 0, 1])
+                $localResult[STMS_STABLE_H] = $localResult[$finished];
+        }
+
+        //
+        // Inform the user as to what was found during those passes
+        reportOneLine($localResult);
+
+    } // now look to check the next pattern
+}
+
+//
+// Convert totals as percentages
+function sortPc($a, $b, $c = null, $d = null, $e = null) {
+    if ($e !== null) {
+        $big = ($a + $b + $c + $d);
+        $mult = $e;
+    } else if ($d !== null) {
+        $big = ($a + $b + $c);
+        $mult = $d;
+    } else {
+        $big = ($a + $b);
+        $mult = $c;
+    }
+
+    if ($big === 0) {
+        $val = "0.00";
+    } else {
+        $pc = 100 / $big;
+        $val1 = round($mult * $pc, 2);
+        $val2 = number_format($val1, 2);
+        if ($val2 === "100.00")
+            $val2 = "100";
+        $val = $val2;
+    }
+    return $val;
+}
+
+//
+// Report stats for multiple passes one one pattern
+function reportOneLine($result) {
+    $s1 = $result[STMS_STABLE];
+    $d1 = $result[STMS_TOODEEP];
+    $c1 = $result[STMS_CYCLE2];
+
+    $s2 = sortPc($s1, $d1, $c1, $s1);
+    $d2 = sortPc($s1, $d1, $c1, $d1);
+    $c2 = sortPc($s1, $d1, $c1, $c1);
+
+    $t1 = $result[STMS_STABLE_T];
+    $h1 = $result[STMS_STABLE_H];
+
+    $t2 = sortPc($t1, $h1, PASSES - $t1 - $h1, $t1);
+    $h2 = sortPc($t1, $h1, PASSES - $t1 - $h1, $h1);
+
+    echo "Pattern: " . implode(",", $result[STMS_INIT_VALS]) . "  ";
+    echo " Stable: " . alignNumber($s2, 5) . "  ";
+    echo "TooDeep: " . alignNumber($d2, 5) . "  ";
+    echo "  Cycle: " . alignNumber($c2, 5) . "  ";
+    echo "      T: " . alignNumber($t2, 5) . "  ";
+    echo "      H: " . alignNumber($h2, 5) . "  ";
+    echo "\n";
+}
+
+//
+// Force values to justify right
+function alignNumber($num, $len) {
+    return (substr("     $num", 0 - $len));
+}
+
+//
+// Exercise says to test things three ways
+test_exampleExercise3n4(UNCLAMPED);
+test_exampleExercise3n4(CLAMPED0);
+test_exampleExercise3n4(CLAMPED1);
+
+?>
--- defines.php
+++ defines.php
@@ -5,11 +5,14 @@ define("USAGE_USE", 0);
 define("USAGE_TEACH", 1);
 define("TAN_ERROR", -1);
 define("UNDEFINED_TXT", 'undefined');
-define('STMS_STABLE', 0);
+define('STMS_STABLE', 0); // nb: STABLE is a cycle that matches last
 define('STMS_TOODEEP', 1);
-define('STMS_CYCLE2', 2);
+define('STMS_CYCLE2', 2); // nb: CYCLE2 means we match pattern before last
 define("UNDEFINED", null);
 define("MATCH_WITH_HAMMING", 1);
 define("MATCH_TAUGHT_ONLY", 2);
+define("UNCLAMPED", null);
+define("CLAMPED0", 0);
+define("CLAMPED1", 1);
 
 ?>
